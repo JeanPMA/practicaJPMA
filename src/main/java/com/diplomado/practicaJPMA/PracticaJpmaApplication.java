9446276f319eb64bc3e459f3705a2993299885d1
Jean Pool <poolgamer0920u@gmail.com>
2023-11-13 15:28:35 -0400

    primer commit

---
+++ src/main/java/com/diplomado/practicaJPMA/PracticaJpmaApplication.java
@@ -0,0 +1,13 @@
+package com.diplomado.practicaJPMA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaJpmaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaJpmaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diplomado/practicaJPMA/PracticaJpmaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diplomado.practicaJPMA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaJpmaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
